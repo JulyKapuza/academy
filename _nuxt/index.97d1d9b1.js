93c809defcb27a633cfd3d2fce689d6c54ca8db5
Juliia Kapuza <105514086+JulyKapuza@users.noreply.github.com>
2023-09-25 17:40:30 +0300

    Updates

--- _nuxt/index.97d1d9b1.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,k as c,j as n}from"./entry.573257a3.js";const o={};function r(t,a){return n(),c("div",null," homePage ")}const _=e(o,[["render",r]]);export{_ as default};
---
+++ _nuxt/index.e452a35f.js
@@ -0,0 +1,1 @@
+import{_ as e,k as n,j as t}from"./entry.4726b6c9.js";const c={};function o(r,s){return t(),n("div",null," Content ")}const a=e(c,[["render",o]]);export{a as default};
